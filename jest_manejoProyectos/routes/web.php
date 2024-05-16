678f62da90b7e6ce66b5034a8a15d9cb33b37bb6
Ernesto <102611496+DiegoCuaycal@users.noreply.github.com>
2024-05-16 10:49:12 -0500

    Plantilla
    Commit

--- jest_manejoProyectos/routes/web.php
+++ jest_manejoProyectos/routes/web.php
@@ -15,3 +15,22 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('pages.index');
+});
+
+Route::get('/login', function () {
+    return view('pages.login');
+});
+
+Route::get('/register', function () {
+    return view('pages.register');
+});
+
+
