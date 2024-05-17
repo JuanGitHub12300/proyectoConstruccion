f4b3658e1a5c32d2c82ed4a2812df8d9eb44542d
Ernesto <102611496+DiegoCuaycal@users.noreply.github.com>
2024-05-17 18:12:40 -0500

    Resolviendo conflictos

--- jest_manejoProyectos/routes/web.php
+++ jest_manejoProyectos/routes/web.php
@@ -3,31 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-<<<<<<< HEAD
-=======
-    return view('welcome');
-});
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
-
-<?php
-
-?>
-
-Route::view('/proyectos', 'proyecto.index');
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
->>>>>>> a6af6385e5acec89c5025414cc84da3810c91741
     return view('pages.index');
 });
 
@@ -39,10 +14,4 @@ Route::get('/register', function () {
     return view('pages.register');
 });
 
-?>
-<<<<<<< HEAD
-=======
-
-
-
->>>>>>> a6af6385e5acec89c5025414cc84da3810c91741
+?> 
